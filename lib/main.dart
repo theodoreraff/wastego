f30004e20fc97b19bbb93bf5b4d778904dd2362b
theodoreraff <theodorusyosia@it.student.pens.ac.id>
2025-04-17 14:52:26 +0700

    feat(theme): apply global Poppins font and white background theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
+
 import 'package:wastego/routes/app_routes.dart';
 import 'package:wastego/core/providers/auth_provider.dart';
 import 'package:wastego/core/providers/schedule_provider.dart';
 import 'package:wastego/core/providers/event_provider.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -25,8 +37,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       initialRoute: AppRoutes.register,
       onGenerateRoute: AppRoutes.generateRoute,
+
+      // Theme Global
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        primarySwatch: Colors.green,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+        ),
+      ),
     );
   }
 }
