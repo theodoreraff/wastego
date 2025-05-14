7a968f8418bb48c9c2bfd38c22fd0e93efc68723
theodores <theodorusyosia15@gmail.com>
2025-05-14 15:13:59 +0700

    chore: Adjust main.dart for app initialization and navigation tweaks

--- lib/main.dart
+++ lib/main.dart
@@ -9,62 +9,7 @@ import 'package:wastego/core/providers/schedule_provider.dart';
 import 'package:wastego/core/providers/event_provider.dart';
 import 'package:wastego/core/providers/notification_provider.dart';
 
-
-// void main() {
-//   SystemChrome.setSystemUIOverlayStyle(
-//     const SystemUiOverlayStyle(
-//       statusBarColor: Colors.white,
-//       statusBarIconBrightness: Brightness.dark,
-//       systemNavigationBarColor: Colors.white,
-//       systemNavigationBarIconBrightness: Brightness.dark,
-//     ),
-//   );
-
-//   runApp(
-//     MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(create: (_) => AuthProvider()),
-//         ChangeNotifierProvider(create: (_) => ScheduleProvider()),
-//         ChangeNotifierProvider(create: (_) => EventProvider()),
-//         ChangeNotifierProvider(create: (_) => NotificationProvider()),
-//       ],
-//       child: const MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       initialRoute: AppRoutes.onboarding,
-//       onGenerateRoute: AppRoutes.generateRoute,
-//       theme: ThemeData(
-//         useMaterial3: true,
-//         scaffoldBackgroundColor: Colors.white,
-//         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-//         primarySwatch: Colors.green,
-//         appBarTheme: const AppBarTheme(
-//           backgroundColor: Colors.white,
-//           foregroundColor: Colors.black,
-//           elevation: 0,
-//           systemOverlayStyle: SystemUiOverlayStyle.dark,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-import 'package:wastego/views/onboarding/onboarding_page_step2.dart';
 void main() {
-  // Menyesuaikan status bar & nav bar agar terlihat bersih
-
-void main() {
-
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.white,
@@ -73,9 +18,6 @@ void main() {
       systemNavigationBarIconBrightness: Brightness.dark,
     ),
   );
-
-
-  runApp(const MyApp());
 
   runApp(
     MultiProvider(
@@ -96,14 +38,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       title: 'WasteGo App',
-      debugShowCheckedModeBanner: false,
-
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.onboarding,
       onGenerateRoute: AppRoutes.generateRoute,
-
       theme: ThemeData(
         useMaterial3: true,
         scaffoldBackgroundColor: Colors.white,
@@ -116,10 +54,6 @@ class MyApp extends StatelessWidget {
           systemOverlayStyle: SystemUiOverlayStyle.dark,
         ),
       ),
-
-      home: const OnboardingPageStep2(),
-
-
     );
   }
 }
